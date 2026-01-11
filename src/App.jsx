3056cc14b64d93a0af0c5b1143ba6bb63909a9e4
Anisul Haque <anisulhaque773@gmail.com>
2026-01-11 01:41:42 +0100

    Passing Props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import FirstComponent from './components/FirstComponent'
 import List from './components/List'
 
 function App() {
-
+  const fruits = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango', 'Pine Apple'];
+  const cities = ['Dhaka','Gazipur','Rajshahi','Sylhet'];
   return (
     <>
       <FirstComponent/>
-      <List></List>
-      <List></List>
+      <List items={fruits} heading = "Fruits"/>
+      <List items={cities} heading = "Cities"></List>
     </>
   )
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,23 +1,15 @@
 import { useState } from "react";
 
 
-const List = () => {
-  const fruits = ['Apple', 'Banana', 'Orange', 'Grapes', 'Mango', 'Pine Apple'];
-
+// eslint-disable-next-line react/prop-types
+const List = ({items = [],heading}) => {
   const [selectedIndex,setSelectedIndex] = useState(-1);
-  // const [name,setName] = useState("");
-
-  // let selectedIndex = -1;
-  // const handleItemPrint = (index) => {
-  //   selectedIndex = index;
-  //   console.log(selectedIndex);
-  // };
-
+  
   return (
     <div>
-      <h1 className="text-xl font-bold">Fruits</h1>
+      <h1 className="text-xl font-bold">{heading}</h1>
       <ul className="pl-6 list-disc text-xl">
-        {fruits.map((fruit, index) => {
+        {items.map((fruit, index) => {
           return (
             <li className={
               selectedIndex === index ? "bg-blue-500 p-3 rounded-sm m-3" : ""
